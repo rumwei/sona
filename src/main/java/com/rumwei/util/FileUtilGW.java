277da866b042caf1b734b065ef7c0c0d852a7134
spark <isguwei@163.com>
2019-04-26 11:04:30 +0800

    append str to file

--- src/main/java/com/rumwei/util/FileUtilGW.java
+++ src/main/java/com/rumwei/util/FileUtilGW.java
@@ -32,8 +32,23 @@ public class FileUtilGW {
         }
     }
 
-    public static void appendStrToFile(String path, String fileName, String content){
+    /*
+    * @Description 用于文件编码格式未知的情况，自动获取文件编码格式，若获取失败，默认按照utf-8来处理
+    * */
+    public static void appendStrToFile(String path, String fileName, String content) throws Exception{
+        String encoding = EncodingDetectGW.getJavaEncode(path+pathSeparator+fileName);
+        if (encoding != null){
+            appendStrToFile(path,fileName,content,encoding);
+        }else {
+            appendStrToFile(path,fileName,content,CharsetEnum.utf8);
+        }
+    }
 
+    /*
+     * @Description 用于文件编码格式已知的情况，直接传入文件编码
+     * */
+    public static void appendStrToFile(String path, String fileName, String content, CharsetEnum charset) throws Exception{
+        appendStrToFile(path,fileName,content,charset.getStrVal());
     }
 
 
@@ -43,6 +58,13 @@ public class FileUtilGW {
 
 
 
+
+
+
+    private static void appendStrToFile(String path, String fileName, String content, String charset) throws Exception{
+        File file = new File(path+pathSeparator+fileName);
+        FileUtils.writeStringToFile(file,content,charset,true);
+    }
 
     private static List<String> readFileToStringList(String path, String fileName, String charset) throws Exception{
         File file = new File(path + pathSeparator + fileName);
